aad5f2b8122263c699cabaaf300998cabeced339
cihancanayyildiz <cihancanayyildiz@gmail.com>
2022-11-20 14:56:41 +0300

    Candy machine changes solana-core-2

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -1,16 +1,87 @@
-import { FC } from "react";
 import {
     Button,
     Container,
     Heading,
-    HStack,
-    Text,
     VStack,
+    Text,
+    HStack,
     Image,
 } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import {
+    FC,
+    MouseEventHandler,
+    useCallback,
+    useEffect,
+    useMemo,
+    useState,
+} from "react";
+import { PublicKey } from "@solana/web3.js";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+    Metaplex,
+    walletAdapterIdentity,
+    CandyMachineV2,
+} from "@metaplex-foundation/js";
+import { useRouter } from "next/router";
 
 const Connected: FC = () => {
+    const { connection } = useConnection();
+    const walletAdapter = useWallet();
+    const [candyMachine, setCandyMachine] = useState<CandyMachineV2>();
+    const [isMinting, setIsMinting] = useState(false);
+
+    const metaplex = useMemo(() => {
+        return Metaplex.make(connection).use(
+            walletAdapterIdentity(walletAdapter)
+        );
+    }, [connection, walletAdapter]);
+
+    useEffect(() => {
+        if (!metaplex) return;
+
+        metaplex
+            .candyMachinesV2()
+            .findByAddress({
+                address: new PublicKey(
+                    "EeT3q7FUxthtG6Gas8jxWRLzNAx7ohMSTYtr4h9CSsz2"
+                ),
+            })
+            .then((candyMachine) => {
+                console.log(candyMachine);
+                setCandyMachine(candyMachine);
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }, [metaplex]);
+
+    const router = useRouter();
+
+    const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
+        async (event) => {
+            if (event.defaultPrevented) return;
+
+            if (!walletAdapter.connected || !candyMachine) {
+                return;
+            }
+
+            try {
+                setIsMinting(true);
+                const nft = await metaplex
+                    .candyMachinesV2()
+                    .mint({ candyMachine });
+
+                console.log(nft);
+                router.push(`/newMint?mint=${nft.nft.address.toBase58()}`);
+            } catch (error) {
+                alert(error);
+            } finally {
+                setIsMinting(false);
+            }
+        },
+        [metaplex, walletAdapter, candyMachine]
+    );
+
     return (
         <VStack spacing={20}>
             <Container>
@@ -28,7 +99,7 @@ const Connected: FC = () => {
                     <Text color="bodyText" fontSize="xl" textAlign="center">
                         Each buildoor is randomly generated and can be staked to
                         receive
-                        <Text as="b"> $BLD</Text> Use your{" "}
+                        <Text as="b"> $BLD</Text>. Use your{" "}
                         <Text as="b"> $BLD</Text> to upgrade your buildoor and
                         receive perks within the community!
                     </Text>
@@ -43,11 +114,14 @@ const Connected: FC = () => {
                 <Image src="avatar5.png" alt="" />
             </HStack>
 
-            <Button bgColor="accent" color="white" maxW="380px">
-                <HStack>
-                    <Text>mint buildoor</Text>
-                    <ArrowForwardIcon />
-                </HStack>
+            <Button
+                bgColor="accent"
+                color="white"
+                maxW="380px"
+                onClick={handleClick}
+                isLoading={isMinting}
+            >
+                <Text>mint buildoor</Text>
             </Button>
         </VStack>
     );
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,12 +2,20 @@ import { HStack, Spacer } from "@chakra-ui/react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { FC } from "react";
 import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
+
+// add this
+const WalletMultiButtonDynamic = dynamic(
+    async () =>
+        (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+);
 
 const NavBar: FC = () => {
     return (
         <HStack width="full" padding={4}>
             <Spacer />
-            <WalletMultiButton
+            <WalletMultiButtonDynamic
                 className={styles["wallet-adapter-button-trigger"]}
             />
         </HStack>
